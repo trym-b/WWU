c568ac222cc8825d23a5856472ce8b2254348475
Vawser <ben.rouncefield@gmail.com>
2025-07-27 02:00:58 +0100

    C# Scripts
    - Added the C# solution so it is simple for others to build and run.

---
+++ # Development/C# Scripts/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EU4_Scratchpad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SetupOutputFolder();
+
+
+        }
+
+        public static void SetupOutputFolder()
+        {
+            var outputDir = Path.Combine(AppContext.BaseDirectory, "Output");
+
+            if (!Directory.Exists(outputDir))
+            {
+                Directory.CreateDirectory(outputDir);
+            }
+        }
+    }
+}
